b519d6ae02c421a33e0410dcf9c293843c7a25df
jasonmueller <jmuel9999@gmail.com>
2021-03-14 10:41:10 -0500

    Created form for header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,17 @@
-var buttonEl = document.querySelector("#save-task");
+
+
+
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() { //Always place this BEFORE the corresponding event listener, otherwise we would call it before the function is defined
+var createTaskHandler = function(event) { //Always place this BEFORE the corresponding event listener, otherwise we would call it before the function is defined
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
 
